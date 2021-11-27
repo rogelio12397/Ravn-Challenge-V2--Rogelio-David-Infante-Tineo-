cd73d04cdd0732ae55aea6117beda263b3fd5626
david <roge_david@outlook.es>
2021-11-27 01:20:47 -0500

    Instalación de Material Boostrap

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 
 import './App.css';
 import CharacterList from './pages/CharactersList';
+import {Header} from './components/common'
 
 function App() {
   return (
-    
-    <div className="App">
-      <CharacterList/>
-     
+    <div>
+      <Header />
+      
+      <div className="col-log-4">Seccion 1</div>
+      <div className="col-log-4">Seccion 2</div>
+      <div className="col-log-4">Seccion 3</div>
+      <div className="App">
+      <CharacterList />
+
+      </div>
     </div>
   );
 }
---
+++ src/components/common/header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header (){
+    return (
+        <section className="header">
+            Hello 
+        </section>
+    )
+}
+
+export default Header;
